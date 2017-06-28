baa0a65a1d1f657aadc5acca87cef968df525412
RedMikePumpkin <michael@malinov.com>
2017-06-28 07:27:05 -0700

    Add files via upload

---
+++ Ada_CurieBot_RC_Joystick/Ada_CurieBot_RC_Joystick.ino
@@ -0,0 +1,258 @@
+/*********************************************************************
+ This is CurieBot RC -- the Arduino 101 based robot using Bluetooth LE
+ Modified to drive a 3-wheeled BLE Robot Rover! by http://james.devi.to
+
+ Pick one up today in the Adafruit shop!
+
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
+
+ MIT license, check LICENSE for more information
+ All text above, and the splash screen below must be included in
+ any redistribution
+*********************************************************************/
+
+#include <string.h>
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+
+  #include <CurieBLE.h>
+  #include <BLEPeripheral.h>
+  #include "BLESerial.h"
+
+  BLESerial ble = BLESerial();
+
+static int lightPin = 13;
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+// And connect 2 DC motors to port M1 & M2 !
+Adafruit_DCMotor *L_MOTOR = AFMS.getMotor(1);
+Adafruit_DCMotor *R_MOTOR = AFMS.getMotor(2);
+
+float leftMotorSpeed = 0.0;
+float rightMotorSpeed = 0.0;
+bool lightState = 0;
+
+#define BLUETOOTH_NAME                 "CurieBot" //Name your RC here
+#define BLE_READPACKET_TIMEOUT         500   // Timeout in ms waiting to read a response
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+// function prototypes over in packetparser.cpp
+uint8_t readPacket(Stream *ble, uint16_t timeout);
+float parsefloat(uint8_t *buffer);
+void printHex(const uint8_t * data, const uint32_t numBytes);
+
+// the packet buffer
+extern uint8_t packetbuffer[];
+
+char buf[60];
+
+/**************************************************************************/
+/*!
+    @brief  Sets up the HW an the BLE module (this function is called
+            automatically on startup)
+*/
+/**************************************************************************/
+void setup(void)
+{
+  //while (!Serial);// For use only while plugged into USB an with Serial Monitor open
+  AFMS.begin();  // create with the default frequency 1.6KHz
+
+  // turn off both motors
+  L_MOTOR->setSpeed(0);
+  R_MOTOR->setSpeed(0);
+  L_MOTOR->run(RELEASE);
+  R_MOTOR->run(RELEASE);
+    
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Bluefruit Robot Controller Example"));
+  Serial.println(F("-----------------------------------------"));
+
+  /* Initialize the module */
+  BLEsetup();
+
+  pinMode(lightPin, OUTPUT);
+
+}
+
+float x, y;
+
+unsigned long lastAccelPacket = 0;
+
+void loop(void)
+{
+  // read new packet data
+  uint8_t len = readPacket(&ble, BLE_READPACKET_TIMEOUT);
+  if (len == 0) return;
+
+  Serial.print("Packet type: ");
+  Serial.println(packetbuffer[1]);
+
+  // Read from Accelerometer input
+  if( accelMode() ) {
+    lastAccelPacket = millis();
+    return;
+  }
+
+  // Stop motors if accelerometer data is turned off (100ms timeout)
+  if((millis() - lastAccelPacket) > 100) {
+    L_MOTOR->run(RELEASE);
+    R_MOTOR->run(RELEASE);
+    leftMotorSpeed = 0.0;
+    rightMotorSpeed = 0.0;
+    lightState = 0;
+    digitalWrite(lightPin, lightState);
+    return;
+  }
+
+  // process buttons
+  buttonMode();
+}
+
+bool accelMode(){
+  if (packetbuffer[1] == 'A') {
+    memcpy(&x, packetbuffer + 2, sizeof x);
+    memcpy(&y, packetbuffer + 6, sizeof y);
+    Serial.print("x = ");
+    Serial.print(x);
+    Serial.print("; y = ");
+    Serial.println(y);
+
+    float leftMotorOutput = 0;
+    float rightMotorOutput = 0;
+
+    // Differential steering code adaptapted from
+    //http://www.impulseadventure.com/elec/robot-differential-steering.html 
+    
+    // Calculate Drive Turn output due to Joystick X input
+    if (y >= 0) {
+      // Forward
+      if (x >= 0) {
+        // Turn right - slow down right motor
+        leftMotorOutput = 1.0;
+        rightMotorOutput = 1.0 - x;
+      } else {
+        // Turn left - slow down left motor
+        leftMotorOutput = 1.0 + x;
+        rightMotorOutput = 1.0;
+      }
+    } else {
+      // Reverse
+      if (x >= 0) {
+        // Turn left - slow down left motor
+        leftMotorOutput = 1.0 - x;
+        rightMotorOutput = 1.0;
+      } else {
+        // Turn right - slow down right motor
+        leftMotorOutput = 1.0;
+        rightMotorOutput = 1.0 + x;    
+      }
+    }
+
+    // Scale Drive output due to Joystick Y input (throttle)
+    leftMotorOutput *= y;
+    rightMotorOutput *= y;
+
+    // Now calculate pivot amount
+    // - Strength of pivot based on Joystick X input
+    // - Blending of pivot vs drive (fPivScale) based on Joystick Y input
+    float fPivLimit = 0.1;
+    float fPivScale = (abs(y)>fPivLimit)? 0.0 : (1.0 - abs(y)/fPivLimit);
+
+    // Calculate final mix of Drive and Pivot
+    leftMotorOutput = (1.0-fPivScale)*leftMotorOutput + fPivScale*x;
+    leftMotorOutput = (1.0-fPivScale)*leftMotorOutput + fPivScale*x;
+
+
+    Serial.print("leftMotorOutput = ");
+    Serial.print(leftMotorOutput);
+    Serial.print("; rightMotorOutput = ");
+    Serial.println(rightMotorOutput);
+
+    // limit acceleration
+    if (leftMotorOutput - leftMotorSpeed > 0.1) {
+      leftMotorOutput = leftMotorSpeed + 0.1;
+    } else if (leftMotorSpeed - leftMotorOutput > 0.1) {
+      leftMotorOutput = leftMotorSpeed - 0.1;
+    }
+    leftMotorSpeed = leftMotorOutput;
+
+    if (rightMotorOutput - rightMotorSpeed > 0.1) {
+      rightMotorOutput = rightMotorSpeed + 0.1;
+    } else if (rightMotorSpeed - rightMotorOutput > 0.1) {
+      rightMotorOutput = rightMotorSpeed - 0.1;
+    }
+    rightMotorSpeed = rightMotorOutput;
+
+    // Convert to Motor Speed to PWM range, and configure motors
+    if (leftMotorSpeed > 0) {
+      L_MOTOR->run( FORWARD );
+      L_MOTOR->setSpeed(leftMotorSpeed * 255); 
+    } else if (leftMotorSpeed < 0) {
+      L_MOTOR->run( BACKWARD );
+      L_MOTOR->setSpeed(-leftMotorSpeed * 255); 
+    } else {
+      L_MOTOR->run( RELEASE );
+      L_MOTOR->setSpeed(0);
+    }
+
+    if (rightMotorSpeed > 0) {
+      R_MOTOR->run( FORWARD );
+      R_MOTOR->setSpeed(rightMotorSpeed * 255); 
+    } else if (rightMotorSpeed < 0) {
+      R_MOTOR->run( BACKWARD );
+      R_MOTOR->setSpeed(-rightMotorSpeed * 255); 
+    } else {
+      R_MOTOR->run( RELEASE );
+      R_MOTOR->setSpeed(0);
+    }
+    return true;
+  }
+  return false;
+}
+
+bool isMoving = false;
+unsigned long lastPress = 0;
+
+bool buttonMode(){
+
+   // Buttons
+  if (packetbuffer[1] == 'B') {
+    uint8_t buttnum = packetbuffer[2] - '0';
+    boolean pressed = packetbuffer[3] - '0';
+
+    Serial.print("Button #"); Serial.print(buttnum);
+    if (pressed) 
+      Serial.println(" pressed");
+    else
+      Serial.println(" released");
+    
+    if (pressed) {
+      if(buttnum == 1) {
+        // toggle the light
+        lightState = !lightState;
+      }
+    }
+    digitalWrite(lightPin, lightState);
+    return true; 
+  }
+
+  return false;
+
+}
+
+void BLEsetup() {
+  // using Curie
+  ble.setLocalName("CurieBot");
+  ble.begin();
+}
---
+++ CurieBot_RC_Remote/CurieBot_BLE_Remote.ino
@@ -0,0 +1,198 @@
+/*
+ * Copyright (c) 2017 Sergey Kiselev and Michael Kiselev.  All rights reserved.
+ *
+ */
+
+/*
+ * Sketch: CurieBot_BLE_Remote.ino
+ *
+ * Description:
+ *  This sketch implements a remote control for CurieBot
+ *  It uses joystick as an input device, and it sends the commands
+ *  to CurieBot using Bluetooth Low Energy Central functionality,
+ *  by writing these commands to Nordic UART TX characteristic
+ *
+ * Notes:
+ *
+ *  - Nordic BLE UART Service: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
+ *  - Nordic BLE UART TX Characteristic: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E
+ *
+ */
+
+#include <CurieBLE.h>
+
+// variables for button
+const int buttonPin = 2;
+const int lightPin = 13;
+const int joystickX = A0;
+const int joystickY = A1;
+int oldButtonState = LOW;
+
+
+void setup() {
+  Serial.begin(115200);
+
+  // configure the button pin as input
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(joystickX, INPUT);
+  pinMode(joystickY, INPUT);
+  pinMode(lightPin, OUTPUT);
+
+  // initialize the BLE hardware
+  BLE.begin();
+
+  Serial.println("BLE Central - CurieBot Control");
+
+  // start scanning for peripherals
+  BLE.scanForUuid("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
+}
+
+void loop() {
+  // check if a peripheral has been discovered
+  BLEDevice peripheral = BLE.available();
+
+  if (peripheral) {
+    // discovered a peripheral, print out address, local name, and advertised service
+    Serial.print("Found ");
+    Serial.print(peripheral.address());
+    Serial.print(" '");
+    Serial.print(peripheral.localName());
+    Serial.print("' ");
+    Serial.print(peripheral.advertisedServiceUuid());
+    Serial.println();
+
+    // stop scanning
+    BLE.stopScan();
+
+    controlBot(peripheral);
+
+    // peripheral disconnected, start scanning again
+    BLE.scanForUuid("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
+  }
+}
+
+void bluefruitSendFloat(BLECharacteristic tx_characteristic, char data_type, float values[])
+{
+  uint8_t packetbuffer[21];
+  uint8_t i, sum = 0;
+  
+  packetbuffer[0] = '!';
+  packetbuffer[1] = data_type;
+  memcpy(packetbuffer + 2, &values[0], 4);
+  memcpy(packetbuffer + 6, &values[1], 4);
+  memcpy(packetbuffer + 10, &values[2], 4);
+
+  for (i = 0; i < 14; i++) sum += packetbuffer[i];
+  packetbuffer[14] = ~sum;
+  
+  tx_characteristic.writeValue(packetbuffer, 15);
+
+}
+
+void bluefruitSendButton(BLECharacteristic tx_characteristic, uint8_t button_number, uint8_t button_state)
+{
+  uint8_t packetbuffer[21];
+  uint8_t sum;
+  
+  packetbuffer[0] = '!';
+  sum = '!';
+  packetbuffer[1] = 'B';
+  sum += 'B';
+  packetbuffer[2] = '0' + button_number;
+  sum += '0' + button_number;
+  packetbuffer[3] = '0' + button_state;
+  sum += '0' + button_state;
+  packetbuffer[4] = ~sum;
+
+  tx_characteristic.writeValue(packetbuffer, 5);
+}
+
+void controlBot(BLEDevice peripheral) {
+  // connect to the peripheral
+  Serial.println("Connecting ...");
+
+  if (peripheral.connect()) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Failed to connect!");
+    return;
+  }
+
+  // discover peripheral attributes
+  Serial.println("Discovering attributes ...");
+  if (peripheral.discoverAttributes()) {
+    Serial.println("Attributes discovered");
+  } else {
+    Serial.println("Attribute discovery failed!");
+    peripheral.disconnect();
+    return;
+  }
+
+  // retrieve the LED characteristic
+  BLECharacteristic txCharacteristic = peripheral.characteristic("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
+
+  if (!txCharacteristic) {
+    Serial.println("Peripheral does not have UART TX characteristic!");
+    peripheral.disconnect();
+    return;
+  } else if (!(txCharacteristic.properties() & BLEWriteWithoutResponse)) {
+    Serial.println("Peripheral does not have a writable UART TX characteristic!");
+    peripheral.disconnect();
+    return;
+  }
+
+  digitalWrite(lightPin, HIGH);
+
+  float curve = -4.0;
+  curve = (curve * -.1) ; // - invert and scale - this seems more intuitive - postive numbers give more weight to high end on output 
+  curve = pow(10, curve); // convert linear scale into lograthimic exponent for other pow function
+
+
+  while (peripheral.connected()) {
+    // while the peripheral is connection
+    float joystick_values[3];
+
+    // read the button pin
+    int buttonState = digitalRead(buttonPin);
+
+    int x = analogRead(joystickX);
+    int y = analogRead(joystickY);
+    Serial.print("joystick X = ");
+    Serial.print(x);
+    Serial.print("; joystick Y = ");
+    Serial.println(y);
+    if (x < 512) {
+      // negative value
+      joystick_values[0] = -pow((511.0 - (float) x) / 512.0, curve);
+    } else {
+      // positive value
+      joystick_values[0] = pow(((float) x - 512.0) / 512.0, curve);
+    }
+    if (y < 512) {
+      // negative value
+      joystick_values[1] = -pow((511.0 - (float) y) / 512.0, curve);
+    } else {
+      // positive value
+      joystick_values[1] = pow(((float) y - 512.0) / 512.0, curve);
+    }
+    joystick_values[2] = 0;
+    bluefruitSendFloat(txCharacteristic, 'A', joystick_values);
+
+    if (oldButtonState != buttonState) {
+      // button state changed
+      if (buttonState == LOW) {
+        bluefruitSendButton(txCharacteristic, 1, 1);
+      } else {
+        bluefruitSendButton(txCharacteristic, 1, 0);
+      }
+      oldButtonState = buttonState;
+    }
+    delay(100);
+  }
+  
+  digitalWrite(lightPin, LOW);
+  Serial.println("Peripheral disconnected");
+}
+
+
+
